5bdcaf816b9fec8055046cc618def8da44b0c952
Artcann <art.cann@orange.fr>
2023-03-14 16:09:18 +0100

    Training Complete on RTX

--- LAB6/imageNN.py
+++ LAB6/imageNN.py
@@ -1,3 +1,5 @@
+import os
+os.environ["tf_gpu_allocator"]="cuda_malloc_async"
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
 from keras.optimizers import Adam
@@ -6,11 +8,15 @@ from keras.utils import to_categorical
 from keras.regularizers import L2
 from keras.losses import SparseCategoricalCrossentropy
 from keras.callbacks import EarlyStopping
+import tensorflow as tf
 import keras_tuner as kt
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import cv2
+import datetime
+
+tf.config.list_physical_devices("GPU")
 
 def model_builder(hp):
     model = Sequential()
@@ -20,12 +26,19 @@ def model_builder(hp):
     model.add(Dropout(0.3))
     model.add(Dense(128, activation='relu')) """
 
-    model.add(Conv2D(filters= 32, kernel_size=(3,3), activation ='relu', input_shape= (32,32,3)))
+    filters_layer_1 = hp.Int("layers_1", min_value=16, max_value=64, step=16)
+    filters_layer_2 = hp.Int("layers_2", min_value=32, max_value=128, step=16)
+    filters_layer_3 = hp.Int("layers_3", min_value=32, max_value=128, step=16)
+
+
+
+    model.add(Conv2D(filters= filters_layer_1, kernel_size=(3,3), activation ='relu', input_shape= (32,32,3)))
     model.add(MaxPooling2D(2,2))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(filters_layer_2, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(filters_layer_3, (3, 3), activation='relu'))
     model.add(Flatten())
+    model.add(Dropout(0.5))
 
     hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
     model.add(Dense(units=hp_units, activation='relu'))
@@ -53,13 +66,16 @@ y_train_encoded = to_categorical(y_train, num_classes=10)
 
 tuner = kt.Hyperband(model_builder,
                      objective='val_accuracy',
-                     max_epochs=10,
+                     max_epochs=20,
                      factor=3,
                      directory='Tuner',
                      project_name='Lab6')
 
-stop_early = EarlyStopping(monitor='val_loss', patience=5)
-tuner.search(X_train_scaled, y_train_encoded, epochs=50, validation_split=0.2, callbacks=[stop_early])
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+stop_early = EarlyStopping(monitor='val_loss', patience=10)
+tuner.search(X_train_scaled, y_train_encoded, epochs=50, validation_split=0.2, callbacks=[stop_early, tensorboard_callback], verbose=2)
 
 best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
 
@@ -70,15 +86,17 @@ is {best_hps.get('learning_rate')}.
 """)
 
 model = tuner.hypermodel.build(best_hps)
-history = model.fit(X_train_scaled, y_train_encoded, epochs=50, validation_split=0.2)
+history = model.fit(X_train_scaled, y_train_encoded, epochs=50, validation_split=0.2, 
+    callbacks=[tensorboard_callback], verbose=2)
 
 val_acc_per_epoch = history.history['val_accuracy']
 best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
 print('Best epoch: %d' % (best_epoch,))
 
-X_test = X_test.astype('float32') / 255.0
-y_test_encoded = to_categorical(y_test, num_classes=10)
+hypermodel = tuner.hypermodel.build(best_hps)
 
-score = model.evaluate(X_test, y_test_encoded, verbose=0)
-print('Test loss:', score[0])
-print(f'Test accuracy: {score[1]*100} %')
+# Retrain the model
+hypermodel.fit(X_train_scaled, y_train_encoded, epochs=best_epoch, validation_split=0.2)
+
+eval_result = hypermodel.evaluate(X_train_scaled, y_train_encoded)
+print("[test loss, test accuracy]:", eval_result)
